6229f193b2660e888753ad9016af72283c4baa25
Shestakov Stanislav <anieb@yandex.ru>
2016-02-05 14:35:52 +0300

    Добавил абстрактные методы.

--- ProgPril_Laba1/Program.cs
+++ ProgPril_Laba1/Program.cs
@@ -8,90 +8,29 @@ namespace ProgPril_Laba1
     {
         static void Main()
         {
-            Console.Write("Введите число генерируемых объектов: ");
-            int ObjNum = Convert.ToInt16(Console.ReadLine());
-            Console.Clear();
-            int[] intArray = new int[ObjNum];
-            Rectangle[] rectArray = new Rectangle[ObjNum];
+            int objCount = 5;
+            int[] intArray = new int[objCount];
+            Rectangle[] rectArray = new Rectangle[objCount];
             Random rnd = new Random();
-            Console.WriteLine("Неотсортированый массив");
-            for (int i=0;i<intArray.Length; i++)
-            {                               
-                intArray[i] = rnd.Next(1, 20);
-                Console.WriteLine(intArray[i]);
-            }
-            Console.WriteLine("Отсортированый массив");
-            Sort(ref intArray);
-            for (int i = 0; i < intArray.Length; i++)
-            {
-                Console.WriteLine(intArray[i]);
-            }
-            Console.ReadLine();
-            Console.Clear();
-
-            Console.WriteLine("Неотсортированый массив");
-            for (int i = 0; i < rectArray.Length; i++)
-            {
-                Rectangle r = new Rectangle(rnd.Next(2, 5), rnd.Next(2, 5));
-                rectArray[i] = r;
-                rectArray[i].draw();
-            }
-
-            Console.WriteLine("Отсортированый массив");
-            SortRect(ref rectArray);
-            for (int i = 0; i < rectArray.Length; i++)
-            {
-                rectArray[i].draw();
-            }
+            for (int i = 0; i < intArray.Length; i++) intArray[i] = rnd.Next(1,15);
+            for (int i = 0; i < rectArray.Length; i++) rectArray[i] = new Rectangle(rnd.Next(2, 10), rnd.Next(2, 10));
+            IntSorter iSort = new IntSorter(intArray);
+            RectSorter rSort = new RectSorter(rectArray);
+            for (int i = 0; i < objCount; i++) Console.WriteLine(iSort.GetElem(i));
+            Console.WriteLine();
             Console.ReadKey();
+            iSort.Sort();
+            for (int i = 0; i < objCount; i++) Console.WriteLine(iSort.GetElem(i));
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Неотсортированый массив:");
+            for (int i = 0; i < objCount; i++) rSort.GetElem(i).draw();
+            rSort.Sort();
+            Console.ReadKey();
+            Console.WriteLine("Отсортированый массив:");
+            for (int i = 0; i < objCount; i++) rSort.GetElem(i).draw();
 
-
-        }
-        static void Sort(ref int[] array)
-        {
-           for(int i=0;i<array.Length;i++)
-            {
-                int min = i;
-                {
-                    for(int j= i;j<array.Length;j++)
-                    {
-                        if(array[i]>array[j])
-                        {
-                            swap(ref array[i],ref array[j]);
-                        }
-                    }
-                }
-            } 
-        }
-
-        static void SortRect(ref Rectangle[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                int min = i;
-                {
-                    for (int j = i; j < array.Length; j++)
-                    {
-                        if (array[i].GET_S > array[j].GET_S)
-                        {
-                            swapRect(ref array[i], ref array[j]);
-                        }
-                    }
-                }
-            }
-        }
-
-        static void swap(ref int a, ref int b)
-        {
-            int c = a;
-            a = b;
-            b = c;
-        }
-        static void swapRect(ref Rectangle a, ref Rectangle b)
-        {
-            Rectangle c = a;
-            a = b;
-            b = c;
+            Console.ReadKey();
         }
     }
 }
--- ProgPril_Laba1/Sorter.cs
+++ ProgPril_Laba1/Sorter.cs
@@ -1,8 +1,93 @@
-﻿namespace ProgPril_Laba1
+﻿using System;
+
+namespace ProgPril_Laba1
 {
     abstract class Sorter
     {
-        public abstract void Compare(int a , int b);
-        public abstract void Sort();
+        protected void doSort()
+        {
+            for(int i = 0; i<GetLenght();i++)
+            {
+                int min = i;
+                for(int j = i+1; j<GetLenght(); j++)
+                {
+                    if (Compare(min, j) == true) Swap(min, j);
+                }
+            }
+        }
+        public abstract int GetLenght();
+        public abstract bool Compare(int a , int b);
+        public abstract void Swap(int i, int j);
+    }
+    class IntSorter:Sorter
+    {
+        int[] array;
+
+        public IntSorter(int[] Array)
+        {
+            this.array = Array;
+        }
+
+        public override int GetLenght()
+        {
+            return array.Length;
+        }
+
+        public override bool Compare(int a, int b)
+        {
+            if (array[a] > array[b]) return true;
+            return false;
+        }
+
+        public override void Swap(int i, int j)
+        {
+            int a = array[i];
+            array[i] = array[j];
+            array[j] = a;
+        }
+        
+        public void Sort()
+        {
+            doSort();
+        }
+        
+        public int GetElem(int a)
+        {
+            return array[a];
+        }
+    }
+    class RectSorter : Sorter
+    {
+        Rectangle[] rectArray;
+
+        public Rectangle GetElem(int a)
+        {
+            return rectArray[a];
+        }
+
+        public RectSorter(Rectangle[] rectArray)
+        {
+            this.rectArray = rectArray;
+        }
+        public override int GetLenght()
+        {
+            return rectArray.Length;
+        }
+
+        public override bool Compare(int a, int b)
+        {
+            if (rectArray[a].GET_S > rectArray[b].GET_S) return true;
+            return false;
+        }
+        public void Sort()
+        {
+            doSort();
+        }
+        public override void Swap(int i, int j)
+        {
+            Rectangle rect = rectArray[i];
+            rectArray[i] = rectArray[j];
+            rectArray[j] = rect;
+        }
     }
 }
