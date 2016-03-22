e88e2a55b50c7649faca4cd3ee0b811a67abf4f2
Shestakov <anieb@yandex.ru>
2016-03-22 13:55:57 +0300

    Events

---
+++ EventsSocialNet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventsSocialNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                    
+            User user1 = new User(1,"Иван","Смирнов");
+            User user2 = new User(2, "Дмитрий", "Дрозд");
+
+            userEvent evn = new userEvent();
+
+            evn.Alert += user1.RecordAlert;
+
+            evn.OnUserActions(user2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ EventsSocialNet/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace EventsSocialNet
+{
+    delegate void UserAction(User person);
+
+    class userEvent
+    {
+        public event UserAction Alert;
+
+        public void OnUserActions(User user)
+        {
+            Alert(user);
+        }
+    }
+
+    class User
+    {      
+        int ID;
+        protected string name, secondName;
+        //private DateTime bDate;
+
+        public User(int ID, string Name, string SecondName)
+        {
+            this.ID = ID;
+            this.name = Name;
+            this.secondName = SecondName;
+        }
+        public string FullName
+        {
+            get { return this.name + " " + this.secondName; }
+        }
+
+        public void PublicationRecord(string record)
+        {
+            Console.WriteLine("Пользователь {0} написал: {1}", this.FullName, record);
+        }
+
+        public void RecordAlert(User person)
+        {
+            Console.WriteLine("Уважаемый {0}", this.FullName);
+            Console.WriteLine("Записи пользователя {0} обновлены!", person.FullName);
+        }
+
+        public void RаteRecord(User user)
+        {
+            Console.WriteLine("Уважаемый {0} {1}", this.name, this.secondName);
+            Console.WriteLine("Пост был оценен пользователем: {0}", user.FullName);
+        }
+    }    
+
+}
