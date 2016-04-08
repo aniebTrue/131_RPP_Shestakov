c00e33252f1384c5c89d393a1f0b334ad4ab9631
Shestakov Stanislav <anieb@yandex.ru>
2016-04-08 08:53:25 +0300

    WPF

---
+++ StoreApp/Item.cs
@@ -0,0 +1,31 @@
+﻿namespace StoreApp
+{
+    interface IItem
+    {
+        int ItemID { get; set; }
+        string ItemName { get; set; }
+        void SellItem(int itemCount);
+        void AddItemOnStore(int itemCount);
+
+    }
+    class Item : IItem
+    {
+        public Item(string name, int ID)
+        {
+            this.ItemID = ID;
+            this.ItemName = name;
+            this.CurentItemCount = 0;
+        }
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int CurentItemCount { get; private set; }
+        public void SellItem(int itemCount)
+        {
+            if (this.CurentItemCount - itemCount >= 0) this.CurentItemCount -= itemCount;
+        }
+        public void AddItemOnStore(int itemCount)
+        {
+            if (itemCount >= 0) this.CurentItemCount += itemCount;
+        }
+    }
+}
---
+++ StoreApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StoreApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Item> lItems = new List<Item>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            lItems.Add(new Item("CPU", 1));
+            lItems.Add(new Item("GPU", 2));
+            lItems.Add(new Item("RAM", 3));
+            ItemsListBox.ItemsSource = lItems;
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            String Name = NewItemNameBox.Text;
+            lItems.Add(new Item(Name, lItems.Last().ItemID+1));
+        }
+    }
+}
