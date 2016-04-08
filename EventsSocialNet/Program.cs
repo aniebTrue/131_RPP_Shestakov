fd80b9d2b1a1f3af2c86ea03dae7d9dc32f3a878
hotwetloli2008 <hotwetloli2008@gmail.com>
2016-04-08 11:36:21 +0300

    ssaa

---
+++ EventsSocialNet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EventsSocialNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            
+            User user1 = new User(1,"Иван","Смирнов");
+            User user2 = new User(2, "Дмитрпий", "Дрозд");
+            User user3 = new User(3, "Ольга", "Власова");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ EventsSocialNet/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace EventsSocialNet
+{
+    class UserAction
+    {
+        public void Alert()
+        {
+            Console.WriteLine("Тревога");
+        }
+    }
+    ф
+
+    class User
+    {
+        int ID;
+        private string name, secondName;
+        //private DateTime bDate;
+
+        public delegate void MethodContainer();
+
+        public event MethodContainer Alert;
+
+        public User(int ID, string Name, string SecondName)
+        {
+            this.ID=ID;
+            this.name=Name;
+            this.secondName=SecondName;
+        }
+        public string FullName
+        {
+            get{return this.name+" "+this.secondName;}
+        }
+
+        public void PublicationRecord(string record)
+        {
+            Console.WriteLine("Пользователь {0} написал: {1}", this.FullName, record);
+            if (Alert != null)
+            {
+                Alert();
+            }
+        }
+        public void RevordAlert(User person)
+        {
+            Console.WriteLine("Уважаемый {0} {1}", this.name, this.secondName);
+            Console.WriteLine("Записи пользователя {0} {1} обновлены!", person.name,person.secondName);
+        }
+
+        public void RаteRecord(User user)
+        {
+            Console.WriteLine("Уважаемый {0} {1}", this.name, this.secondName);
+            Console.WriteLine("Пост был оценен пользователем: {0}",user.FullName);
+        }
+    }
+}
