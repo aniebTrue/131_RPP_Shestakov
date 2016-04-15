07111ca73ee2130a5ff3511bdf50fa10e39b611d
Shestakov <anieb@yandex.ru>
2016-04-15 14:20:23 +0300

    MVC

---
+++ MvcProjectBilling/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using ProjectBilling.Business.MVC;
+
+namespace ProjectBilling.UI.MVC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IProjectsController _controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _controller
+                = new ProjectsController(new ProjectsModel());
+        }
+
+        private void ShowProjectsButton_Click(object sender,
+            RoutedEventArgs e)
+        {
+            _controller.ShowProjectsView(this);
+        }
+    }
+}
---
+++ StoreApp/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace StoreApp
+{
+    interface ICustomer
+    {
+        string Name { get; set; }
+        bool isOpt;
+        bool isUsual;
+        
+    }
+    class Customer:ICustomer
+    {
+
+    }
+}
